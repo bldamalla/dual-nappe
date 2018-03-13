087d2775107080f984798009f69e5f4d29281a8b
bldamalla <bonleifamalla@gmail.com>
2018-03-13 08:11:09 +0800

    Add assets and learn page

---
+++ assets/trivia.js
@@ -0,0 +1,72 @@
+var trivia = [
+{
+	'title': 'Quote of the Day',
+	'text': '"It is a true man\'s part not to err, but it is also noble of a man to perceive his error." <br> Apollonius of Perga, father of conics',
+	'link': 'https://www.brainyquote.com/quotes/apollonius_of_tyana_729720'
+},
+{
+	'title': 'Conic Cubes or Cubic Cones',
+	'text': 'In his two solutions to the Greek problem of doubling the cube, Menaechmus first introduced conic sections in 360-350 BC.',
+	'link': 'http://sites.math.rutgers.edu/~cherlin/History/Papers1999/schmarge.html'
+},
+{
+	'title': 'Etymologists',
+	'text': 'Perga coined the names of the different conics, thus giving them Greek etymologies. Parabola derives from parabola - meaning applied, ellipse derives from ellipsis - meaning deficit, and the hyperbola derives from hyperbola - meaning excess.',
+	'link': ['https://www.etymonline.com/word/ellipse', 'https://www.etymonline.com/word/parabola', 'https://www.etymonline.com/word/hyperbola']
+},
+{
+	'title': 'Orbital Origins',
+	'text': 'German astronomer Johannes Kepler established that the orbits of planets are ellipses with the sun at a focus.',
+	'link': 'https://math.berkeley.edu/~robin/Kepler/index.htm'
+},
+{
+	'title': 'Pi is Everywhere',
+	'text': 'The area of an ellipse can be calculated by multiplying the constant pi with the lengths of the semimajor and semiminor axes.',
+	'link': 'https://faculty.math.illinois.edu/~mjunge/40611/Pr1.pdf'
+},
+{
+	'title': 'Madness of a Method',
+	'text': 'Developed in the 1970s by Shor, Yudin, and Nemirovsky, the ellipsoid method is a more efficient (but slower) method for minimizing convex methods, which finds optimal solutions after certain steps.',
+	'link': 'https://web.stanford.edu/class/ee364b/lectures/ellipsoid_method_slides.pdf'
+},
+{
+	'title': 'The Mound in Washington, D.C.',
+	'text': 'In the capital of the United States, Charles L\'Enfant designed different shapes throughout the city. One of these is the park only known as the Ellipse, with a semimajor axis of 529.13 feet and a semiminor axis of 451.42 feet.',
+	'link': 'http://faculty.evansville.edu/ck6/ellipse.pdf'
+},
+{
+	'title': 'Zonal Marking',
+	'text': 'Fuzzy calls? Well, blame Fresnel (partially). He found a calculation that helps calculate how out of phase radio waves are between a transmitter and a receive. These zones come in the form of ellipses, with a few exceptions dependent on the obstructions for radio waves.',
+	'link': 'http://www.digitalairwireless.com/wireless-blog/recent/fresnel-zones-what-are-they-and-why-are-they-so-important.html'
+},
+{
+	'title': 'Off the Pascal',
+	'text': 'Degenerations, or rather a case limited by some constraint to simplify a certain problem, of Pascal\'s theorem can be used to define a parabola (this can also be said about ellipses).',
+	'link': 'https://www-m10.ma.tum.de/foswiki/pub/Lehre/WS0809/GeometrieKalkueleWS0809/ch1.pdf'
+},
+{
+	'title': 'Galilei\'s Throws',
+	'text': 'Galileo realized that the horizontal and vertical vectors of projectile motion were separate, and realized that it is of a parabolic motion.',
+	'link': 'http://galileo.rice.edu/lib/student_work/experiment95/paraintr.html'
+},
+{
+	'title': 'Suspended Parabolas',
+	'text': 'With a main or central span of 1991 meters and towers of height 282 meters, the Akashi Kaikyo bridge at the Akashi Strait is the world\'s longest suspension bridge. Would you solve how high the focus would be?',
+	'link': 'https://www.britannica.com/topic/Akashi-Strait-Bridge'
+},
+{
+	'title': 'Time Teller',
+	'text': 'Pelekinons are an ancient form of sundials. Throughout the day, the rays of the sun strike the pole of the sundial at different parts. They form a shadow that changes length, and the furthest points of each shadow form a portion of a hyperbola.',
+	'link': 'http://www2.mae.ufl.edu/~uhk/PELEKINON.pdf'
+},
+{
+	'title': 'Atom Smashing',
+	'text': 'Rutherford conducted an experiment in regards of the forces between incoming alpha particle and a positive and compact nucleus. The said orbit and or trajectory of the alpha particle was found to be a hyperbola with an eccentricity of the reciprocal of the sine function of theta over two, given thst theta is the angle of deflection.',
+	'link': 'https://www.researchgate.net/profile/Masatsugu_Suzuki/publication/303983573_Rutherford_scattering_and_hyperbola_orbit/links/5761544308ae244d0372c364/Rutherford-scattering-and-hyperbola-orbit.pdf'
+},
+{
+	'title': 'Tangency Trivia',
+	'text': 'The tangent to a point on the hyperbola is the angle bisector between the segments from the point to each focus.',
+	'link': 'https://www.emathzone.com/tutorials/geometry/condition-for-line-tangent-to-hyperbola.html '
+}
+]
